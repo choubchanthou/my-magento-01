f127aab2d195a6c2e3b99c601d691a036d05d3a5
Choub Chanthou <chanthou@EMs-MacBook-Pro.local>
2018-12-03 15:44:20 +0700

    Test

---
+++ Chanthou/AIS/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2018 chanthou.com. All rights reserved.
+
+ * @author Choub Chanthou <chanthou_choub@hotmail.com>
+ */
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Chanthou_AIS',
+    __DIR__
+);
